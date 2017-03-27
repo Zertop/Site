b39358c1f7e4b6b24551db78b053b3b4e9f72b97
Zertop <info@zertop.com>
2017-03-27 20:41:05 +0200

    First Site Commit

--- releases/parse.js
+++
@@ -1,6 +0,0 @@
-//Parse latest github release and serve to client
-$.getJSON('https://api.github.com/repos/Zertop/LineTool/releases/latest?' + (new Date()).getTime(), function(data) {
-    document.getElementById("downloadBlock").src = data.assets[0].browser_download_url;
-});
-
-console.log("Test");
